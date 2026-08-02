3ea8a765b5eb23d9f8aba7906a507a5cdc47ca57
Earl Jordan Parreno <morigan2005@gmail.com>
2026-08-03 03:01:57 +0800

    Fix Anki import WASM file missing in production by adding outputFileTracingIncludes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["sql.js", "anki-apkg-export"],
+    outputFileTracingIncludes: {
+      "/api/anki/import": ["./node_modules/sql.js/dist/sql-wasm.wasm"],
+    },
   },
 };
 module.exports = nextConfig;
